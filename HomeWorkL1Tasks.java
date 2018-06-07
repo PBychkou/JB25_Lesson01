e37542b2ba741c6bed095ce2c6675db6e93b7642
PBychkou <orangemoon@list.ru>
2018-06-07 09:54:23 +0300

    Add files via upload

---
+++ HomeWorkL1Tasks.java
@@ -0,0 +1,41 @@
+package by.epam.JB25.lesson1_hw;
+
+public class HomeWorkL1Tasks {
+
+	public static void main(String[] args) {
+		
+		//Task #2
+		System.out.println("Bychkou Pavel");
+		System.out.println("");
+		
+		
+		//Task #3
+		System.out.println("Пройдет много лет, и полковник Аурлеано Буэндиа, сидя у стены в ожидании расстрела, вспомнит тот далекий вечер, когда отец взял его с собой посмотреть на лед. Макондо было тогда небольшим селением с двумя десятками хижин, выстроенных из глины и бамбука на берегу реки, которая мчала свои прозрачные воды...");
+		System.out.println("");
+		
+		//Task #4
+		int a = 2;
+		int b = 4;
+		double hyp, s;
+		
+		hyp = Math.sqrt(a * a + b * b);
+		s = (a * b) / 2; 
+		
+		System.out.println("a = " + a + ", b = " + b + "\n hyp = " + hyp + " s = " + s);
+		System.out.println("");
+		
+		//Task #5
+		int var1, var2, var3, var4;
+		var1 = 4;
+		var2 = 3;
+		var3 = 7;
+		var4 = 9;
+		
+		int sum = var1 + var2 + var3 + var4;
+		
+		System.out.println("Sum = " + sum);		
+		
+
+	}
+
+}
